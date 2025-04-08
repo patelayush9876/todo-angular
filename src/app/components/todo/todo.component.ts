82a99a436f94f2b408b4701b456c7717e4e019f2
AYUSH PATEL <149704996+patelayush9876@users.noreply.github.com>
2025-04-08 23:12:46 +0530

    mark as done added

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,7 +1,11 @@
-// todo.component.ts
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
+interface Todo {
+  text: string;
+  completed: boolean;
+}
 
 @Component({
   standalone: true,
@@ -12,16 +16,20 @@ import { FormsModule } from '@angular/forms';
 })
 export class TodoComponent {
   todoText = '';
-  todos: string[] = [];
+  todos: Todo[] = [];
 
   addTodo() {
     if (this.todoText.trim()) {
-      this.todos.push(this.todoText.trim());
+      this.todos.push({ text: this.todoText.trim(), completed: false });
       this.todoText = '';
     }
   }
 
   deleteTodo(index: number) {
     this.todos.splice(index, 1);
+  }
+
+  toggleCompleted(index: number) {
+    this.todos[index].completed = !this.todos[index].completed;
   }
 }
